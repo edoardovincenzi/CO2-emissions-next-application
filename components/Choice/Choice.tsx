2d0b6e6c2867188a7d6ff7f70071db5cb94be125
edoardovincenzi <edoardo.vincenzi95@gmail.com>
2022-07-16 17:35:04 +0200

    resolve problem from MUI and css module

--- components/Choice/Choice.tsx
+++ components/Choice/Choice.tsx
@@ -3,7 +3,7 @@ import styles from './Choice.module.css';
 
 const Choice: React.FC<{ text: string }> = ({ text }) => {
   return (
-    <Paper elevation={3} className={styles.paper}>
+    <Paper elevation={3} className={styles.paperChoice}>
       {text}
     </Paper>
   );
